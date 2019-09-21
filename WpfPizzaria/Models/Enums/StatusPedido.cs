39a1510ad2b995e115b1b5983601a10d75abf66f
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2019-09-21 17:14:16 -0300

    Alterando model pedido add enum statusPedido

---
+++ WpfPizzaria/Models/Enums/StatusPedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPizzaria.Models.Enums
+{
+    enum StatusPedido : int
+    {
+        PagamentoPendente =1,
+        Processando =2,
+        Enviado =3,
+        Entregue =4
+    }
+}
--- WpfPizzaria/Models/Venda.cs
+++ WpfPizzaria/Models/Venda.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfPizzaria.Models.Enums;
 
 namespace WpfPizzaria.Models {
     class Venda {
@@ -10,12 +11,14 @@ namespace WpfPizzaria.Models {
         public Cliente Cliente { get; set; }
         public Funcionario Funcionario { get; set; }
         public List<ItemVenda> ProdutosVenda { get; set; } //Produtos desta venda
+        public StatusPedido statusPedido { get; set; }
         public DateTime CriadoEm { get; set; }
 
         public Venda() {
             Cliente = new Cliente();
             Funcionario = new Funcionario();
             CriadoEm = DateTime.Now;
+            statusPedido = StatusPedido.PagamentoPendente;
         }
     }
 }
