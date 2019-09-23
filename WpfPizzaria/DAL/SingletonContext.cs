babb01b94fc6485a9007f1ae46d9b6a5907a4727
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2019-09-23 14:47:17 -0300

    Criando classe SingletonContext

---
+++ WpfPizzaria/DAL/SingletonContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfPizzaria.Models;
+
+namespace WpfPizzaria.DAL
+{
+    class SingletonContext
+    {
+        //Atibuto do tipo context
+        private static Context ctx;
+
+        //Construtor
+        public SingletonContext()
+        {
+                
+        }
+
+        //Método que verefica se o contexto já está instanciado
+        //Caso já esteja nao deixa instanciar novamente e retorna o contexto já instanciado
+        public static Context GetInstance()
+        {
+            if (ctx == null)
+            {
+                ctx = new Context();
+            }
+            return ctx;
+        }
+    }
+}
