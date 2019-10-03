04b7a4519699aa6d3aadc5d25090d451aca6be77
RenanDLRS <51766433+RenanDLRS@users.noreply.github.com>
2019-10-03 16:50:15 -0300

    Add files via upload

---
+++ WpfPizzaria/Views/FrmListarFuncionario.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfPizzaria.DAL;
+using WpfPizzaria.Models;
+
+namespace WpfPizzaria.Views
+{
+    /// <summary>
+    /// Interaction logic for FrmListarFuncionario.xaml
+    /// </summary>
+    public partial class FrmListarFuncionario : Window
+    {
+        public FrmListarFuncionario()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmListarFuncionario_Loaded(object sender, RoutedEventArgs e)
+        {
+            dtaFuncionarios.ItemsSource = FuncionarioDAO.ListarFuncionarios();
+        }
+
+        private void BtnVoltar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnAlterar_Click(object sender, RoutedEventArgs e)
+        {
+            FrmCadastrarFuncionario frmCadastrarFuncionario = new FrmCadastrarFuncionario((Funcionario)dtaFuncionarios.SelectedValue);
+            frmCadastrarFuncionario.ShowDialog();
+
+            dtaFuncionarios.Items.Refresh();
+        }
+
+        private void BtnDeletar_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Tem certeza que deseja excluir?", "Confirmar exclusão", MessageBoxButton.YesNo, MessageBoxImage.Exclamation) == MessageBoxResult.Yes)
+            {
+                FuncionarioDAO.DeletarFuncionario((Funcionario)dtaFuncionarios.SelectedValue);
+            }
+
+            dtaFuncionarios.Items.Refresh();
+        }
+    }
+}
